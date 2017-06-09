6a97c45aa6a5e064cd551e35200d365e80c62cc7
Pavel <pshynin@yahoo.com>
2017-06-09 13:40:05 -0400

    step 2

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
 /**
  * Created by pshynin on 6/9/17.
  */
 public class FirstTest {
+
+    @Test
+    public void testOne() {
+        System.out.println("Test");
+    }
 }
