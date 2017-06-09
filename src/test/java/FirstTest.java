745c4f1bc43b58cfcde8c42aa3f60137bfacef22
Pavel <pshynin@yahoo.com>
2017-06-09 15:05:40 -0400

    step 3

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,9 +4,10 @@ import org.junit.Test;
  * Created by pshynin on 6/9/17.
  */
 public class FirstTest {
+    public static final String TEST = "Test";
 
     @Test
     public void testOne() {
-        System.out.println("Test");
+        System.out.println(TEST);
     }
 }
