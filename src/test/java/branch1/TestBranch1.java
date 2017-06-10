d5122d3b40fc505ba2a0ee100cebfd11907f13d4
Pavel <pshynin@yahoo.com>
2017-06-10 15:12:59 -0400

    branch 1.0 commit 1

---
+++ src/test/java/branch1/TestBranch1.java
@@ -0,0 +1,15 @@
+package branch1;
+
+import org.junit.Test;
+
+/**
+ * Created by pshynin on 6/10/17.
+ */
+public class TestBranch1 {
+
+    @Test
+    public void testBranch1() {
+        System.out.println("This is branch 1.0");
+    }
+
+}
