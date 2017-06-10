6c62963f10b557850540e4532a3e2fabe7cbf3b2
Pavel <pshynin@yahoo.com>
2017-06-10 15:59:23 -0400

    commit 2 branch 1

--- src/test/java/branch1/Test01.java
+++ src/test/java/branch1/Test01.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 /**
  * Created by pshynin on 6/10/17.
  */
-public class TestBranch1 {
+public class Test01 {
 
     @Test
     public void testBranch1() {
-        System.out.println("This is branch 1.0");
+        System.out.println("This is branch 1.0 Test 1");
     }
 
 }
---
+++ src/test/java/branch1/Test02.java
@@ -0,0 +1,15 @@
+package branch1;
+
+import org.junit.Test;
+
+/**
+ * Created by pshynin on 6/10/17.
+ */
+public class Test02 {
+
+    @Test
+    public void testBranch1() {
+        System.out.println("This is branch 1.0 Test 2");
+    }
+
+}
