9538ea2ffd526e2c9be256373fe0d4d41554ef08
Pavel <pshynin@yahoo.com>
2017-06-10 15:16:57 -0400

    branch 3.0 commit 1

---
+++ src/test/java/branch3/TestBranch3.java
@@ -0,0 +1,15 @@
+package branch3;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by pshynin on 6/10/17.
+ */
+public class TestBranch3 {
+
+    @Test
+    public void testBranch3() {
+        System.out.println("This is branch 1.0");
+    }
+
+}
