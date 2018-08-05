6de803592a75b328d6f16c3cc3a6a5954e82069b
Pavel <pshynin@yahoo.com>
2018-08-04 21:13:13 -0400

    added background implementation

---
+++ core/src/com/flappybird/game/Background.java
@@ -0,0 +1,44 @@
+package com.flappybird.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Background {
+    class BGPicture {
+        private Texture tx;
+        private Vector2 pos;
+
+        public BGPicture(Vector2 pos) {
+            this.tx = new Texture("back.png");
+            this.pos = pos;
+        }
+    }
+
+    private int speed;
+    private BGPicture[] backs;
+
+    public Background() {
+        speed = 4;
+        backs = new BGPicture[2];
+        backs[0] = new BGPicture(new Vector2(0, 0));
+        backs[1] = new BGPicture(new Vector2(800, 0));
+    }
+
+    public void render(SpriteBatch batch) {
+        for (BGPicture pic : backs) {
+            batch.draw(pic.tx, pic.pos.x, pic.pos.y);
+        }
+    }
+
+    public void update() {
+        for (BGPicture pic : backs) {
+            pic.pos.x -= speed;
+        }
+
+        if (backs[0].pos.x < -800) {
+            backs[0].pos.x = 0;
+            backs[1].pos.x = 800;
+        }
+    }
+}
--- core/src/com/flappybird/game/FlappyBirdGame.java
+++ core/src/com/flappybird/game/FlappyBirdGame.java
@@ -3,31 +3,34 @@ package com.flappybird.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBirdGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Background bg;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        bg = new Background();
+    }
+
+    @Override
+    public void render() {
+        update();
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        bg.render(batch);
+        batch.end();
+    }
+
+    private void update() {
+        bg.update();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
